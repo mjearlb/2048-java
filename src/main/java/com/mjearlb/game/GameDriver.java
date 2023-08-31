c7da6a50c1de64851dff65f3d412fde8fcfea967
Milo Jefferson Bauman <mjb01161@uga.edu>
2023-08-31 16:11:50 -0400

    Added Maven project structure

---
+++ src/main/java/com/mjearlb/game/GameDriver.java
@@ -0,0 +1,11 @@
+package com.mjearlb.game;
+
+/**
+ * Hello world!
+ *
+ */
+public class GameDriver {
+    public static void main(String[] args) {
+        System.out.println( "Hello World!" );
+    } // main
+} // GameDriver
