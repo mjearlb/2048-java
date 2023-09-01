6d184b04d0c268de3ba1d28166972211ecbbb96a
Milo Jefferson Bauman <mjb01161@uga.edu>
2023-09-01 13:50:25 -0400

    Changed main to use Game

--- src/main/java/com/mjearlb/game/GameDriver.java
+++ src/main/java/com/mjearlb/game/GameDriver.java
@@ -1,11 +1,13 @@
 package com.mjearlb.game;
 
+import com.mjearlb.game.Game;
+
 /**
  * Hello world!
  *
  */
 public class GameDriver {
     public static void main(String[] args) {
-        System.out.println( "Hello World!" );
+        Game game = new Game();
     } // main
 } // GameDriver
