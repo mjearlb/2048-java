cadef089695a8828a2fc1758085e96d13e759853
Milo Jefferson Bauman <mjb01161@uga.edu>
2023-09-01 13:33:47 -0400

    Created Game.java to contain the game's guts

---
+++ src/main/java/com/mjearlb/game/Game.java
@@ -0,0 +1,17 @@
+package com.mjearlb.game;
+
+/**
+ * This contains methods used for the game 2048.
+ */
+public class Game {
+
+    private int score;
+
+    /**
+     * Creates a new {@code Game}.
+     */
+    public Game() {
+        this.score = 0;
+    } // Game
+
+} // Game
