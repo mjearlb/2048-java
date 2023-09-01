01811eb5d141faf8cd18ba0fcdb3d329e1285727
Milo Jefferson Bauman <mjb01161@uga.edu>
2023-09-01 13:42:25 -0400

    Added startGame() and printBoard()

--- src/main/java/com/mjearlb/game/Game.java
+++ src/main/java/com/mjearlb/game/Game.java
@@ -6,12 +6,38 @@ package com.mjearlb.game;
 public class Game {
 
     private int score;
+    private char[][] board;
 
     /**
      * Creates a new {@code Game}.
      */
     public Game() {
         this.score = 0;
+        startGame();
     } // Game
+
+    /**
+     * Starts the game.
+     */
+    private void startGame() {
+        this.board = new char[4][4];
+        for (int i = 0; i < this.board.length; i++) {
+            for (int j = 0; j < this.board[i].length; j++) {
+                this.board[i][j] = " ";
+            } // for
+        } // for
+        printBoard();
+    } // startGame
+
+    /**
+     * Prints the current board.
+     */
+    public void printBoard() {
+        for (int i = 0; i < this.board.length; i++) {
+            for (int j = 0; j < this.board[i].length; j++) {
+                System.out.print(this.board[i][j]);
+            } // for
+        } // for
+    } // printBoard
 
 } // Game
