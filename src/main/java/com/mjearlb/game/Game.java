94b078a14e85b019f8cfa794e1292de6efa01007
Milo Jefferson Bauman <mjb01161@uga.edu>
2023-09-02 13:03:35 -0400

    Created board border. printBoard also now clears the terminal window.

--- src/main/java/com/mjearlb/game/Game.java
+++ src/main/java/com/mjearlb/game/Game.java
@@ -6,7 +6,7 @@ package com.mjearlb.game;
 public class Game {
 
     private int score;
-    private char[][] board;
+    private String[][] board;
 
     /**
      * Creates a new {@code Game}.
@@ -20,12 +20,14 @@ public class Game {
      * Starts the game.
      */
     private void startGame() {
-        this.board = new char[4][4];
+        this.board = new String[4][4];
         for (int i = 0; i < this.board.length; i++) {
             for (int j = 0; j < this.board[i].length; j++) {
-                this.board[i][j] = ' ';
+                this.board[i][j] = "   ";
             } // for
         } // for
+        this.board[0][1] = " 2 ";
+        this.board[1][3] = " 2 ";
         printBoard();
     } // startGame
 
@@ -33,12 +35,26 @@ public class Game {
      * Prints the current board.
      */
     public void printBoard() {
+        clearScreen();
+        System.out.println("+---------------+");
+        System.out.println("|     2048!     |");
+        System.out.println("+---------------+");
         for (int i = 0; i < this.board.length; i++) {
+            System.out.print("|");
             for (int j = 0; j < this.board[i].length; j++) {
-                System.out.print(this.board[i][j]);
+                System.out.print(this.board[i][j] + "|");
             } // for
             System.out.println();
+            System.out.println("+---------------+");
         } // for
     } // printBoard
+
+    /**
+     * Clears the terminal window.
+     */
+    public void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    } // clearScreen
 
 } // Game
