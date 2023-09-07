e89d897ad39b4cfa7ebcb4a19ae766a7cfbab699
Milo Jefferson Bauman <mjb01161@uga.edu>
2023-09-07 18:16:16 -0400

    Added game loop

--- src/main/java/com/mjearlb/game/GameDriver.java
+++ src/main/java/com/mjearlb/game/GameDriver.java
@@ -2,12 +2,21 @@ package com.mjearlb.game;
 
 import com.mjearlb.game.Game;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class GameDriver {
+
+    private static Scanner keyboard = new Scanner(System.in);
+
     public static void main(String[] args) {
         Game game = new Game();
+
+        while (true) {
+            game.slideHandler(keyboard.nextLine());
+        } // while
     } // main
 } // GameDriver
